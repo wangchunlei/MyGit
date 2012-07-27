bc47b89ff2e83639f97877c58ad4bd4fe8887862
wangchunlei <wcl_orange@msn.com>
2012-07-27 08:53:51 +0800

    add

---
+++ Node.js/My Start/nodeStart/server.js
@@ -0,0 +1,18 @@
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+	function onRequest(request, response){
+		var pathname=url.parse(request.url).pathname;
+		console.log("Request for "+pathname+" received.");
+		
+		var content=route(handle,pathname);
+		
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write(content);
+		response.end();
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
+}
+exports.start = start;

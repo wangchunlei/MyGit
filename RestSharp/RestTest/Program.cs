caee494ce2fcb5a07197216a958820ed5f7eae97
wangchunlei <wcl_orange@163.com>
2013-09-26 17:45:12 +0800

    commit

--- RestSharp/RestTest/Program.cs
+++ RestSharp/RestTest/Program.cs
@@ -40,61 +40,81 @@ namespace RestTest
         }
         static void Main(string[] args)
         {
-            System.Threading.Tasks.Task.Factory.StartNew(() =>
-                {
-                    System.Threading.Thread.Sleep(1000);
-                   WebApi();
-                });
-            var thread = new System.Threading.Thread(WebApi);
-            thread.Start();
-            System.Threading.Thread.Sleep(2000);
-          WebApi();
-            //var url =
-            //    string.Format(
-            //        "http://192.168.100.254?username={0}&password={1}&password_enc={2}&login={3}&login_type={4}&uri={5}&password_type={6}",
-            //        "wangchunleird", "lx314114", "bHgzMTQxMTQ%3D&newpassword_enc=", "1", "login", "aHR0cDovLzE5Mi4xNjguNjAuNjUv", "normal"
-            //        );
-            //foreach (var @interface in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()
-            //    .SelectMany(c => c.GetIPProperties().UnicastAddresses)
-            //    .Where(c => c.Address.AddressFamily == AddressFamily.InterNetwork && c.IsDnsEligible))
-            //{
-            //    var ipendPoint = new IPEndPoint(@interface.Address, 0);
-            //    Console.WriteLine(ipendPoint);
+            var client = new RestClient("http://192.168.100.254");
+            var request = new RestRequest("", Method.POST);
 
-            //    var serverPoint = ServicePointManager.FindServicePoint(new Uri("http://192.168.100.254"));
-            //    serverPoint.BindIPEndPointDelegate = delegate(
-            //        ServicePoint servicePoint,
-            //        IPEndPoint remoteEndPoint,
-            //        int retryCount)
-            //        {
-            //            return ipendPoint;
-            //        };
-            //    serverPoint.ConnectionLeaseTimeout = 0;
-            //    var req = WebRequest.Create(url) as HttpWebRequest;
-            //    req.KeepAlive = false;
-            //    req.Method = "Post";
-            //    req.Timeout = 1000 * 30;
-            //    //req.ReadWriteTimeout = 1000 * 10;
+            var username = args[0];
+            var password = args[1];
+            request.AddParameter("username", username);
+            request.AddParameter("password", password);
+            request.AddParameter("password_enc", "bHgzMTQxMTQ%3D&newpassword_enc=");
+            request.AddParameter("login", "1");
+            request.AddParameter("login_type", "login");
+            request.AddParameter("uri", "aHR0cDovLzE5Mi4xNjguNjAuNjUv");
+            request.AddParameter("password_type", "normal");
 
-            //    try
-            //    {
-            //        using (var response = req.GetResponse())
-            //        {
-            //            using (var stream = response.GetResponseStream())
-            //            {
-            //                content = new StreamReader(stream).ReadToEnd();
-            //            }
-            //        }
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        Console.WriteLine(ex.Message);
-            //    }
+            var response = client.Execute(request);
 
-            //}
-
+            var content = response.Content;
+            Console.WriteLine(content);
             Console.WriteLine("完成");
-            //Console.ReadKey(false);
+
+
+            ////  System.Threading.Tasks.Task.Factory.StartNew(() =>
+            ////      {
+            ////          System.Threading.Thread.Sleep(1000);
+            ////         WebApi();
+            ////      });
+            ////  var thread = new System.Threading.Thread(WebApi);
+            ////  thread.Start();
+            ////  System.Threading.Thread.Sleep(2000);
+            ////WebApi();
+            //  //var url =
+            //  //    string.Format(
+            //  //        "http://192.168.100.254?username={0}&password={1}&password_enc={2}&login={3}&login_type={4}&uri={5}&password_type={6}",
+            //  //        "wangchunleird", "lx314114", "bHgzMTQxMTQ%3D&newpassword_enc=", "1", "login", "aHR0cDovLzE5Mi4xNjguNjAuNjUv", "normal"
+            //  //        );
+            //  //foreach (var @interface in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()
+            //  //    .SelectMany(c => c.GetIPProperties().UnicastAddresses)
+            //  //    .Where(c => c.Address.AddressFamily == AddressFamily.InterNetwork && c.IsDnsEligible))
+            //  //{
+            //  //    var ipendPoint = new IPEndPoint(@interface.Address, 0);
+            //  //    Console.WriteLine(ipendPoint);
+
+            //  //    var serverPoint = ServicePointManager.FindServicePoint(new Uri("http://192.168.100.254"));
+            //  //    serverPoint.BindIPEndPointDelegate = delegate(
+            //  //        ServicePoint servicePoint,
+            //  //        IPEndPoint remoteEndPoint,
+            //  //        int retryCount)
+            //  //        {
+            //  //            return ipendPoint;
+            //  //        };
+            //  //    serverPoint.ConnectionLeaseTimeout = 0;
+            //  //    var req = WebRequest.Create(url) as HttpWebRequest;
+            //  //    req.KeepAlive = false;
+            //  //    req.Method = "Post";
+            //  //    req.Timeout = 1000 * 30;
+            //  //    //req.ReadWriteTimeout = 1000 * 10;
+
+            //  //    try
+            //  //    {
+            //  //        using (var response = req.GetResponse())
+            //  //        {
+            //  //            using (var stream = response.GetResponseStream())
+            //  //            {
+            //  //                content = new StreamReader(stream).ReadToEnd();
+            //  //            }
+            //  //        }
+            //  //    }
+            //  //    catch (Exception ex)
+            //  //    {
+            //  //        Console.WriteLine(ex.Message);
+            //  //    }
+
+            //  //}
+
+            //  Console.WriteLine("完成");
+            //  //Console.ReadKey(false);
         }
 
         private static string CallAPI(string value)
